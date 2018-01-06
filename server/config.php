9ce5d98ae6afe569f0feca8f74f967751d2171b0
Vy Nghia <dora.nghia@gmail.com>
2018-01-06 13:03:03 +0700

    Update config.php

--- server/config.php
+++ server/config.php
@@ -6,16 +6,16 @@ error_reporting(0); // Turn off error reporting text
 define('SECRET', 'NghiaisGay');
 
 // Group ID
-define('GroupID', '1918111098516614');
+define('GroupID', '000000000000000');
 
 // Your Token (Please using iPhone/Android/iOS access_token)
-define('access_token', 'EAAAAUaZA8jlABAFIOzBPtx28eGiGat4bE8PKiJ0AFAROIDrri1w5tMRM2vcuw6PnlP1SdQGo8EaxOy3CmgZAg0BQKvPr40Ns79EbTh3uByeS7mG0t2I5HRxwhqlMFczmaZBZCPqtBdvLvf3WQRIbVZBql8yk7zvsZD');
+define('access_token', 'XXXXXXXXXXXXXXX');
 
 // Arrray Points Count
 $pnt = array(
-		'points_per_post' => 5,
-		'points_per_comment' => 2,
-		'points_per_commented' => 2,
-		'points_per_reaction' => 1,
-		'points_per_reacted' => 1
-		);
+	'points_per_post' => 5,
+	'points_per_comment' => 2,
+	'points_per_commented' => 2,
+	'points_per_reaction' => 1,
+	'points_per_reacted' => 1
+);
